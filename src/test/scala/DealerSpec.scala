d55e89d65fbdc50c93a0ae71e16dc7d0750cc6ce
locojay <locojaydev@gmail.com>
2016-06-29 21:37:32 -0400

    cleanup

--- src/test/scala/DealerSpec.scala
+++ src/test/scala/DealerSpec.scala
@@ -6,10 +6,10 @@ import holdem._
 class DealerSpec extends FunSpec with Matchers {
 
   it("should be able to evaluate who the winner is") {
-    // val hand = CardsParser.parse("2d2s")
-    // val community_cards = CardsParser.parse("2c3d4h")
-    // val other_players_cards = List(CardsParser.parse("3s4s"))
-    // Dealer.evaluate(hand, community_cards, other_players_cards) shouldEqual -1
+    val hand = CardsParser.parse("2dAs")
+    val community_cards = CardsParser.parse("2c3d4h")
+    val other_players_cards = List(CardsParser.parse("3s4s"))
+    Dealer.evaluate(hand, community_cards, other_players_cards) shouldEqual -1
 
     val hand1 = CardsParser.parse("AsKs")
     val community_cards1 = CardsParser.parse("AdKcKh")
--- src/test/scala/HandSpec.scala
+++ src/test/scala/HandSpec.scala
@@ -11,20 +11,20 @@ class HandSpec extends FunSpec with Matchers {
      Hand strength is valued on a scale of 1 to 7462,
      where 1 is a Royal Flush and 7462 is unsuited 7-5-4-3-2
      */
-    // val royal_flush = CardsParser.parse("TsJsQsKsAs")
-    // Hand.evaluate(royal_flush) shouldEqual 1
+    val royal_flush = CardsParser.parse("TsJsQsKsAs")
+    Hand.evaluate(royal_flush) shouldEqual 1
 
-    // val straight_flush = CardsParser.parse("9sTsJsQsKs")
-    // Hand.evaluate(straight_flush) shouldEqual 2
+    val straight_flush = CardsParser.parse("9sTsJsQsKs")
+    Hand.evaluate(straight_flush) shouldEqual 2
 
-    // val straight_flush_queen_high = CardsParser.parse("8s9sTsJsQs")
-    // Hand.evaluate(straight_flush_queen_high) shouldEqual 3
+    val straight_flush_queen_high = CardsParser.parse("8s9sTsJsQs")
+    Hand.evaluate(straight_flush_queen_high) shouldEqual 3
 
 
-    // val seven_high= CardsParser.parse("7s6h4c3h2s")
-    // Hand.evaluate(seven_high) shouldEqual 7461
-    // val seven_high1 = CardsParser.parse("7s5h4c3h2s")
-    // Hand.evaluate(seven_high1) shouldEqual 7462
+    val seven_high= CardsParser.parse("7s6h4c3h2s")
+    Hand.evaluate(seven_high) shouldEqual 7461
+    val seven_high1 = CardsParser.parse("7s5h4c3h2s")
+    Hand.evaluate(seven_high1) shouldEqual 7462
 
     Hand.evaluate(CardsParser.parse("AdKcKhAsKs")) shouldEqual 179
 
